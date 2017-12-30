b0a7c1c3bfa577419e7efba77e52f07d388ee2a3
James <DarkHeart527@users.noreply.github.com>
2017-12-30 15:22:03 -0800

    Add files via upload

--- MPAL.js
+++ MPAL.js
@@ -5,6 +5,7 @@ if (document.getElementsByName('mpal')[0].attributes.action.value == 'allow') {
 		setExeArea: function(target) {
 			this.targetSetup = target;
 		},
+		artists: [],
 		interval: '',
 		currentArtist: 1,
 		currentSong: 1,
@@ -26,7 +27,9 @@ if (document.getElementsByName('mpal')[0].attributes.action.value == 'allow') {
 					var trackNameElement = JSPlugs.newElement('div');
 					trackNameElement.onclick = function() {
 						// Create a Play on click function
+						JSP('#audioPort2').volume = 0;
 						JSP('#audioPort1').src = this.attributes.src;
+						JSP('#audioPort2').src = this.attributes.src;
 						MPAL.playing = true;
 						JSP('#playpause').src = 'pause.png';
 					}
@@ -85,12 +88,89 @@ if (document.getElementsByName('mpal')[0].attributes.action.value == 'allow') {
 			if (this.playing == true) {
 				this.playing = false;
 				JSP('#audioPort1').pause();
+				JSP('#audioPort2').pause();
 			} else {
 				this.playing = true;
 				JSP('#audioPort1').play();
+				JSP('#audioPort2').play();
+			}
+		},
+		frequencyArray: [],
+		freqSave: {},
+		frequencyReceiver: function() {
+			if (this.startedF == false) {
+				this.freqSave.audio = JSP('#audioPort2');
+				this.freqSave.context = new AudioContext();
+				this.freqSave.canvas = JSP('#canvasBackground');
+				this.freqSave.analyser = this.freqSave.context.createAnalyser();
+				this.freqSave.ctx = this.freqSave.canvas.getContext('2d');
+				this.freqSave.source = this.freqSave.context.createMediaElementSource(this.freqSave.audio);
+				this.freqSave.source.connect(this.freqSave.analyser);
+				this.startedF = true;
+			} else {
+				this.freqSave.fbc_array = new Uint8Array(this.freqSave.analyser.frequencyBinCount);
+				this.freqSave.analyser.getByteFrequencyData(this.freqSave.fbc_array);
+				this.freqSave.ctx.clearRect(0, 0, this.freqSave.canvas.width, this.freqSave.canvas.height);
+				this.freqSave.ctx.fillStyle = '#00CCFF';
+				bars = 100;
+				for (let i = 0; i < bars; i++) {
+					bar_x = i * 3;
+					bar_width = 2;
+					bar_height = -(this.freqSave.fbc_array[i] / 2);
+					this.freqSave.ctx.fillRect(bar_x, this.freqSave.canvas.height, bar_width, bar_height);
+				}
+			}
+		},
+		startedF: false,
+		visualizer: function(mode, func) {
+			if (mode == 'on') {
+				JSP('#canvasBackground').style.display = 'block';
+				this.visualizerInterval = setInterval( function() {
+					MPAL.frequencyReceiver();
+					func();
+				}, 50);
+			} else if (mode == 'off') {
+				JSP('#canvasBackground').style.display = 'none';
+				clearInterval(this.visualizerInterval);
 			}
 		},
 	}
 } else {
 	console.error('Music Player AniLive IS NOT FOR USE, please DELETE THIS SCRIPT IMMEDIATLY! \nMPAL.js');
 }
+
+/*	Visusalizer Reference
+
+		var audio = new Audio();
+		audio.src = 'https://darkheart527.github.io/darkheartprod/beats/Flames.mp3';
+		audio.autoplay = true;
+		audio.controls = true;
+		var context, analyser, canvas, ctx, source;
+		function init() {
+			document.body.appendChild(audio);
+			context = new AudioContext();
+			analyser = context.createAnalyser();
+			canvas = document.getElementById('analyser');
+			ctx = canvas.getContext('2d');
+			source = context.createMediaElementSource(audio);
+			source.connect(analyser);
+			analyser.connect(context.destination);
+			frameLooper(context);
+		}
+		
+		function frameLooper(context) {
+			window.requestAnimationFrame(frameLooper);
+			fbc_array = new Uint8Array(analyser.frequencyBinCount);
+			analyser.getByteFrequencyData(fbc_array);
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			ctx.fillStyle = '#00CCFF';
+			bars = 100;
+			for (let i = 0; i < bars; i++) {
+				bar_x = i * 3;
+				bar_width = 2;
+				bar_height = -(fbc_array[i] / 2);
+				ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+			}
+		}
+		init();
+*/
