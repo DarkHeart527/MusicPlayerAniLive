51abd448a8021fc82f2e2866e80ae61ddf47b856
James <DarkHeart527@users.noreply.github.com>
2018-01-06 12:58:14 -0800

    Add files via upload

--- MPAL.js
+++ MPAL.js
@@ -1,5 +1,9 @@
 
 if (document.getElementsByName('mpal')[0].attributes.action.value == 'allow') {
+	var audioPort1 = new Audio();
+	audioPort1.id = 'audioPort1';
+	audioPort1.autoplay = true;
+	document.body.appendChild(audioPort1);
 	var MPAL = {
 		targetSetup: '',
 		setExeArea: function(target) {
@@ -7,12 +11,15 @@ if (document.getElementsByName('mpal')[0].attributes.action.value == 'allow') {
 		},
 		artists: [],
 		interval: '',
-		currentArtist: 1,
-		currentSong: 1,
+		starInterval: '',
+		currentArtistDisplay: 0,
+		currentArtist: 0,
+		currentSong: 0,
+		started: false,
 		currentSongName: '',
-		currentArtistName: '',
+		currentArtistDisplayName: '',
 		updateDisplay: function(change) {
-			var artist = MPAL.artists[MPAL.currentArtist - 1];
+			var artist = MPAL.artists[MPAL.currentArtistDisplay];
 			var artistSimplify = '';
 			if (change == 'artist') {
 				for (let i = 0; i < artist.length; i++) {
@@ -50,31 +57,35 @@ if (document.getElementsByName('mpal')[0].attributes.action.value == 'allow') {
 				musicList.appendChild(artistElement);
 				for (let i2 = 0; i2 < artist.length(); i2++) {
 					var track = artist[Object.keys(artist)[i2]];
+					var trackNumber = i2;
 					var trackName = Object.keys(artist)[i2];
 					var trackElement = JSPlugs.newElement('div');
 					var trackNameElement = JSPlugs.newElement('div');
+					trackNameElement.attributes.trackNumber = trackNumber;
 					trackNameElement.onclick = function() {
 						// Create a Play on click function
-						//JSP('#audioPort2').volume = 0;
+						MPAL.currentArtist = MPAL.currentArtistDisplay;
 						JSP('#audioPort1').src = this.attributes.src;
-						JSP('#audioPort2').src = this.attributes.src;
 						MPAL.playing = true;
-						JSP('#playpause').src = 'pause.png';
-						MPAL.visualizer('on');
+						JSP('.playpausePicture').src = 'buttonPictures/pause.png';
+						if (this.started == false) {
+							this.started = true;
+						}
+						MPAL.currentSong = this.attributes.trackNumber;
 					}
 					JSP('#nextArtist').onclick = function() {
-						if (MPAL.currentArtist == MPAL.artists.length) {
-							MPAL.currentArtist = 1;
+						if (MPAL.currentArtistDisplay == MPAL.artists.length) {
+							MPAL.currentArtistDisplay = 0;
 						} else {
-							MPAL.currentArtist++;
+							MPAL.currentArtistDisplay++;
 						}
 						MPAL.updateDisplay('artist');
 					}
 					JSP('#previousArtist').onclick = function() {
-						if (MPAL.currentArtist == 1) {
-							MPAL.currentArtist = MPAL.artists.length;
+						if (MPAL.currentArtistDisplay == 0) {
+							MPAL.currentArtistDisplay = MPAL.artists.length;
 						} else {
-							MPAL.currentArtist--;
+							MPAL.currentArtistDisplay--;
 						}
 						MPAL.updateDisplay('artist');
 					}
@@ -86,15 +97,25 @@ if (document.getElementsByName('mpal')[0].attributes.action.value == 'allow') {
 					trackElement.appendChild(trackNameElement);
 				}
 			}
+			if (location.protocol == 'file:') { // Check If Page Is Local File
+				var disabledText = JSPlugs.newElement('p');
+				disabledText.style.color = 'red';
+				disabledText.innerHTML = 'Disabled - Reason: Local File Has No Permission, Must Be Online';
+				JSP('#visualizerSelectionNote').appendChild(disabledText);
+				//JSP('#visualizerSelection').disabled = true;
+			}
 			JSP('#artistNameDisplay').innerHTML = Object.keys(tracks.artists)[0];
-			JSP('.artist')[0].style.display = 'block';
+			JSP('.artist')[0].style.display = 'block'; // Show Display Of First Artist Page
 			JSP('#playpause').onclick = function() {MPAL.playPause();};
-			JSP('#audioPort1').onloadedmetadata = function() {
+			JSP('#audioPort1').onloadedmetadata = function() { // Execute When File Is Loaded
 				JSP('#progress').attributes.length = this.duration;
+				
 				MPAL.interval = setInterval( function() {
 					JSP('#progress').style.width = (Number(JSP('#audioPort1').currentTime) / Number(JSP('#audioPort1').duration) * 100) + '%';
 					var currentTime = Math.floor(JSP('#audioPort1').currentTime);
 					var duration = Math.floor(JSP('#audioPort1').duration);
+					/* ------------------------------------ */
+					// Convert To Readable Time
 					var audio = {
 						initialSeconds: currentTime,
 						initialSeconds2: duration,
@@ -123,21 +144,51 @@ if (document.getElementsByName('mpal')[0].attributes.action.value == 'allow') {
 					if (audio.second2 < 10) {
 						audio.second2 = '0' + audio.second2;
 					}
+					/* ------------------------------------ */
 					duration = audio.minute2 + ':' + audio.second2;
 					currentTime = audio.minute + ':' + audio.second;
 					JSP('#progressText').innerHTML = currentTime + ' / ' + duration;
+					if (JSP('#audioPort1').ended == true) { // Check If File Duration Is Reached
+						var mainSource = tracks.mainSource; // Get The Main Source And Simplify It
+						var artistName = Object.keys(tracks.artists)[MPAL.currentArtist];
+						var artistLength = Object.keys(tracks.artists[artistName]).length - 1;
+						if (MPAL.currentSong == artistLength) {
+							MPAL.currentSong = 0;
+						} else {
+							MPAL.currentSong += 1;
+						}
+						console.log(MPAL.currentSong + ':' + artistLength);
+						var newSongName = tracks.artists[artistName];
+						newSongName = Object.keys(newSongName)[MPAL.currentSong];
+						var newSongSource = tracks.artists[artistName];
+						newSongSource = newSongSource[newSongName].source;
+						newSongSource = mainSource + artistName + '/' + newSongSource;
+						JSP('#audioPort1').src = newSongSource;
+					}
 				});
 			}
 		},
 		playPause: function() {
 			if (this.playing == true) {
-				this.playing = false;
 				JSP('#audioPort1').pause();
-				JSP('#audioPort2').pause();
+				JSP('.playpausePicture').src = 'buttonPictures/play.png';
+				this.playing = false;
 			} else {
 				this.playing = true;
+				JSP('.playpausePicture').src = 'buttonPictures/pause.png';
+				if (this.started == false) {
+					MPAL.currentSong = 0;
+					var artist = MPAL.currentArtistDisplay;
+					var trackNum = 0;
+					var trackArtist = Object.keys(tracks.artists)[artist];
+					var trackName = Object.keys(tracks.artists[trackArtist])[trackNum];
+					var track = tracks.artists[trackArtist];
+					track = track[trackName].source;
+					MPAL.currentArtist = MPAL.currentArtistDisplay;
+					JSP('#audioPort1').src = tracks.mainSource + trackArtist + '/' + track;
+					this.started = true;
+				}
 				JSP('#audioPort1').play();
-				JSP('#audioPort2').play();
 			}
 		},
 		customSettings: {
@@ -145,44 +196,53 @@ if (document.getElementsByName('mpal')[0].attributes.action.value == 'allow') {
 		},
 		frequencyArray: [],
 		freqSave: {},
-		frequencyReceiver: function() {
+		frequencyReceiver: function(type) { // Receive Frequencies And Create A Visualizer
 			if (this.startedF == false) {
-				this.freqSave.audio = JSP('#audioPort2');
+				this.freqSave.audio = JSP('#audioPort1');
 				this.freqSave.context = new AudioContext();
 				this.freqSave.canvas = JSP('#canvasBackground');
 				this.freqSave.analyser = this.freqSave.context.createAnalyser();
 				this.freqSave.ctx = this.freqSave.canvas.getContext('2d');
 				this.freqSave.source = this.freqSave.context.createMediaElementSource(this.freqSave.audio);
 				this.freqSave.source.connect(this.freqSave.analyser);
-				this.startedF = true;
-			} else {
 				this.freqSave.fbc_array = new Uint8Array(this.freqSave.analyser.frequencyBinCount);
 				this.freqSave.analyser.getByteFrequencyData(this.freqSave.fbc_array);
-				this.freqSave.ctx.clearRect(0, 0, this.freqSave.canvas.width, this.freqSave.canvas.height);
-				this.freqSave.ctx.fillStyle = this.customSettings.mainColor;
-				bars = 100;
-				for (let i = 0; i < bars; i++) {
-					bar_x = i * 3;
-					bar_width = 2;
-					bar_height = -(this.freqSave.fbc_array[i] / 2);
-					this.freqSave.ctx.fillRect(bar_x, this.freqSave.canvas.height, bar_width, bar_height);
-				}
-				if (JSP('#audioPort1').currentTime.toFixed(0) != JSP('#audioPort2').currentTime.toFixed(0)) {
-					console.info('Display time was not equal to audio time ' + JSP('#audioPort1').currentTime + ' - ' + JSP('#audioPort2').currentTime + ' equaling out to ' + JSP('#audioPort1').currentTime);
-					JSP('#audioPort2').currentTime = JSP('#audioPort1').currentTime;
-				}
+				this.startedF = true;
+			}
+			switch(type) {
+				case 'frequencyBars':
+					this.freqSave.ctx.clearRect(0, 0, this.freqSave.canvas.width, this.freqSave.canvas.height);
+					this.freqSave.ctx.fillStyle = this.customSettings.mainColor;
+					bars = 100;
+					for (let i = 0; i < bars; i++) {
+						bar_x = i * 3;
+						bar_width = 2;
+						bar_height = -(this.freqSave.fbc_array[i] / 2);
+						this.freqSave.ctx.fillRect(bar_x, this.freqSave.canvas.height, bar_width, bar_height);
+					}
+				break;
+				case 'stars': // Get 3 In Array 0
+					var freq = this.freqSave.fbc_array[4];
+					console.error('This is not made yet - stars\nLine 224');
+				break;
 			}
 		},
 		startedF: false,
-		visualizer: function(mode, func) {
+		visualizer: function(mode, type) {
+			if (type == 'noVisual') {
+				mode = 'off';
+			}
 			if (mode == 'on') {
+				clearInterval(this.visualizerInterval);
 				JSP('#canvasBackground').style.display = 'block';
 				this.visualizerInterval = setInterval( function() {
-					MPAL.frequencyReceiver();
-					if (func != '' | func != undefined) {
-						//func();
-					}
+					MPAL.frequencyReceiver(type);
 				});
+				if (type == 'stars') {
+					this.starInterval = setInterval( function() {
+						
+					}, 15);
+				}
 			} else if (mode == 'off') {
 				JSP('#canvasBackground').style.display = 'none';
 				clearInterval(this.visualizerInterval);
---
+++ updateNotes.js
@@ -0,0 +1,10 @@
+// Update Notes
+var updates = {
+	'1.0.0 BETA': 'This is the start of the new and improv(ed, ing) Music Player now know as "Music Player AniLive."' +
+	'I expect this Music Player to look the best and perform the best. I also expect to have many different' +
+	' visualizers that work with the frequencies a little more to add nice animations, and to have a new live' +
+	' mode to play music live at any DJ type party or whatever. Among custom uploads, custom Music Videos, ' +
+	'custom animation, custom colors, and much more, I expect this to be my very best project or creation within' +
+	' programming itself. Please Note: Uploads ONLY save to your local storage and may have limits, it is' +
+	' reccomeneded that you use sources INSTEAD of uploads to have the best experience.',
+}
